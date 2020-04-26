bcc68dcb1811bf33c20cc7ee58e85555945241ae
cyp <chenyongping001@qq.com>
2020-04-26 21:29:58 +0800

    alien_invasion

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings():
     def __init__(self):
         """初始化游戏的设置"""
         # 屏幕的设置
-        self.screen_width = 1024
-        self.screen_height = 768
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
