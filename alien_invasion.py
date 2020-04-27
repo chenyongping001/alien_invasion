0be0268efee221b89fef7e597b75cab8cd0e6ec9
chen-work-pc <chenyongping001@qq.com>
2020-04-27 09:34:50 +0800

    16_over

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,16 +5,19 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
     """初始化游戏并创建一个屏幕对象"""
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     # 开始游戏的主循环
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
+
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,15 +2,29 @@ import sys
 import pygame
 
 
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        # # 向右移动飞船
+        # ship.rect.centerx +=1
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
 def check_events(ship):
     # 监视键盘和鼠标事件
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # 向右移动飞船
-                ship.rect.centerx +=1
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
